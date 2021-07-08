ff69f64dffd125818633ff22bc90246f8c7c2390
Alif-Tegor-Sf <Aliffarsiky1717@gmail.com>
2021-07-08 20:50:41 +0700

    Uploading 09

---
+++ app/Http/Controllers/MahasiswaController.php
@@ -0,0 +1,151 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Models\Mahasiswa;
+use App\Models\Kelas;
+use PDF;
+class MahasiswaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $mahasiswa=Mahasiswa::with('kelas')->get(); 
+        // Mengambil semua isi tabel
+        $paginate=Mahasiswa::orderBy('nim','desc')->paginate(1);
+        return view('mahasiswa.index',compact('mahasiswa','paginate'))->with('i',(request()->input('page',1)-1)*5);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $kelas=Kelas::all();
+        return view('mahasiswa.create',compact('kelas'));
+    }
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate(['nim'=>'required'
+        ,'nama'=>'required'
+        ,'kelas_id'=>'required'
+        ,'jurusan'=>'required'
+        ,'no_handphone'=>'required'
+        ,'email'=>'required'
+        ,'tanggal_lahir'=>'required'
+        ]);//fungsieloquentuntukmenambahdata
+        Mahasiswa::create($request->all());
+        $image = $request->file('foto');
+        if($image)
+        {
+           $image_name = $request->file('foto')->store('images','public');
+        }
+       Mahasiswa::create([
+
+       'nim'=>$request->nim
+       ,'nama'=>$request->nama
+       ,'kelas_id'=>$request->kelas_id
+       ,'jurusan'=>$request->jurusan
+       ,'no_handphone'=>$request->no_handphone
+       ,'email'=>$request->email
+       ,'tanggal_lahir'=>$request->tanggal_lahir
+       ,'foto'=>$image_name
+
+       ]);
+        //jikadataberhasilditambahkan,akankembalikehalamanutama
+        return redirect()->route('mahasiswa.index')->with('success','Mahasiswa Berhasil Ditambahkan');
+    }
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($nim)
+    {
+        $kelas=Kelas::all();
+        return view('mahasiswa.edit',compact('mahasiswa','kelas'));
+    }
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($nim)
+    {
+        $mahasiswa=Mahasiswa::find($nim);
+        return view('mahasiswa.edit',compact('mahasiswa'));
+    }
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $nim)
+    {
+        $request->validate(['nim'=>'required'
+        ,'nama'=>'required'
+        ,'kelas_id'=>'required'
+        ,'jurusan'=>'required'
+        ,'no_handphone'=>'required'
+         ,'email'=>'required'
+        ,'tanggal_lahir'=>'required'
+        ]);
+        Mahasiswa::find($nim)->update($request->all());
+        $mhs = Mahasiswa::find($nim);
+        $mhs->nim = $request->nim;
+        $mhs->nama = $request->nama;
+        $mhs->kelas_id = $request->kelas_id;
+        $mhs->jurusan = $request->jurusan;
+        $mhs->no_handphone = $request->no_handphone;
+        $mhs->email = $request->email;
+        $mhs->tanggal_lahir = $request->tanggal_lahir;
+         if($mhs->foto && file_exists(storage_path('app/public/'.$mhs->foto)))
+        {
+            Storage::delete('public/'.$mhs->foto);
+        }
+        $image_name = $request->file('foto')->store('images','public');
+        $mhs->foto = $image_name;
+        $mhs->save();
+        return redirect()->route('mahasiswa.index')->with('success','Mahasiswa Berhasil Diupdate');
+    }
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($nim)
+    {
+        Mahasiswa::find($nim)->delete();
+        return redirect()->route('mahasiswa.index')->with('success','Mahasiswa Berhasil  Dihapus');
+    }
+    public function nilai($nim)
+    {
+        $mhs = Mahasiswa::find($nim);
+        //$jajal = $mhs->matakuliah;
+        //$kelas = $mhs->kelas->nama_kelas;
+         return view('mahasiswa.nilai',compact('mhs'));
+    }
+
+    public function cetak_pdf($id)
+    {
+        $mhs = Mahasiswa::find($id);
+        $pdf = PDF::loadview('mahasiswa.cetak_pdf',compact('mhs'));
+        return $pdf->stream();
+    }
+}
---
+++ app/Models/Mahasiswa.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Mahasiswa;
+
+class Mahasiswa extends Model
+{
+    use HasFactory;
+    protected $table='mahasiswas';
+    protected $primaryKey='nim';
+    protected $incerement = false;
+    protected $fillable = ['nim','nama','kelas_id','jurusan','no_handphone','email','tanggal_lahir','foto'];
+    
+
+    public function kelas()
+    {
+        return $this->belongsTo(kelas::class);
+    }
+} 
